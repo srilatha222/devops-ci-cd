75cec0932a68b83351a2cdefbfed0bb6c51ab89b
srilatha222 <vallemonisrilatha841@gmail.com>
2022-02-02 14:56:52 +0530

    updated

--- src/main/java/com/myapp/spring/DevopsCiCdApplication.java
+++ src/main/java/com/myapp/spring/DevopsCiCdApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DevopsCiCdApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello");
 		SpringApplication.run(DevopsCiCdApplication.class, args);
 	}
 
